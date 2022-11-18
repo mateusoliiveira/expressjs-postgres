110e00f036cdaa3626e9eae157e935a876e6c4b3
Mateus Morais <81278450+mateusoliiveira@users.noreply.github.com>
2022-11-18 12:15:03 -0300

    fix: remove router files

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import cors from "cors";
 import express from "express";
 import pg from "pg";
 import errorHandlerMiddleware from "./shared/errors/error-handler";
-import routes from "./shared/infra/http/routes";
 
 const pool = new pg.Pool()
 const app = express();
@@ -10,17 +9,30 @@ const port = process.env.PORT || 3333;
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);
-app.use(errorHandlerMiddleware)
+const racesController = new RacesController();
 
-app.get("/", async (req, res) => {
+app
+  .get("/", async (req, res) => {
   const { rows } = await pool.query("SELECT NOW()");
   res.send(`DB is on and the time of it is ${rows[0].now}`);
 });
-
-app.get('/test', (req, res) => {
+app
+  .get('/test', (req, res) => {
   res.send({ greeting: 'Hello, world!' });
 })
+app
+  .get(
+    '/schedule',
+    racesController
+      .index);
+
+app
+  .get(
+    '/schedule/show:link?',
+    racesController
+      .show);
+
+app.use(errorHandlerMiddleware)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
